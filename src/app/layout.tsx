3223d7d12175f14d94f00b44e8f5cda5eb19aa3b
YuichirouSeitoku <kazeso.kazewo@gmail.com>
2024-08-24 23:04:29 +0900

    ヘッダーを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import ButtonAppBar from "../components/ButtonAppBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ButtonAppBar />
         <AppRouterCacheProvider>
           {children}
         </AppRouterCacheProvider>
